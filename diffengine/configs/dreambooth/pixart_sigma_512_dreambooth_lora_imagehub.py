12a3c6b5bbd8dad81aecee45364d1f59a746da47
okotaku <to78314910@gmail.com>
2024-04-27 08:09:37 +0000

    Update config

--- diffengine/configs/dreambooth/pixart_sigma_512_dreambooth_lora_imagehub.py
+++ diffengine/configs/dreambooth/pixart_sigma_512_dreambooth_lora_imagehub.py
@@ -4,14 +4,13 @@ with read_base():
     from .._base_.datasets.imagehub_dreambooth import *
     from .._base_.default_runtime import *
     from .._base_.models.pixart_sigma_512_lora import *
-    from .._base_.schedules.diffusion_1k_baseline import *
+    from .._base_.schedules.diffusion_1k import *
 
 model.update(
+    input_perturbation_gamma=0.1,
     enable_vb_loss=False,
-    #weight_dtype="bf16"
-    )
+    weight_dtype="bf16")
 
 optim_wrapper.update(
     optimizer=dict(lr=1e-4),
-    #clip_grad=dict(max_norm=1.0),
-    )
+    clip_grad=dict(max_norm=1.0))
